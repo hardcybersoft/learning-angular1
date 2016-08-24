188c8b7349441e356f33d07dc65981844be0358d
zero <zero@localhost.localdomain>
2016-08-24 16:01:40 -0300

    Ejemplos de ngRoute

---
+++ Rutas/run.sh
@@ -0,0 +1,16 @@
+#instalación
+#sudo npm install -g lite-server
+lite-server
+
+# Otras formas para correr el proyecto:
+
+#1)
+#google-chrome --allow-file-access-from-files
+
+#2)
+#python -m SimpleHTTPServer 8000
+
+#3)
+#php -S localhost:8000
+
+
